67718504c6347fa901864a7a83f4c1e3b675d4f8
gemesel <gemesel_bonnyel@yahoo.de>
2020-08-04 11:15:34 +0200

    first refactor

--- FizzBuzz/src/fizzbuzz.java
+++ FizzBuzz/src/fizzbuzz.java
@@ -2,18 +2,32 @@
 public class fizzbuzz {
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 100; i++) {
-			if(i % 3 == 0 && i % 5 == 0) {
-				System.out.println("FizzBuzz");
-			}else if(i % 3 == 0) {
-				System.out.println("Fizz");
-			}else if(i % 5 == 0) {
-				System.out.println("Buzz");
-			}else {
-				System.out.println(i);
+		for (int i = 0; i < 100; i++) {
+			String temp = "";
+			temp += dividedThree(i);
+			temp += dividedFive(i);
+			if (temp.equals("")) {
+				temp += i;
 			}
+			System.out.println(temp);
+
 		}
 
 	}
 
+	public static String dividedThree(int n) {
+		if (n % 3 == 0) {
+			return "Fizz";
+		} else {
+			return "";
+		}
+	}
+
+	public static String dividedFive(int n) {
+		if (n % 5 == 0) {
+			return "Buzz";
+		} else {
+			return "";
+		}
+	}
 }
